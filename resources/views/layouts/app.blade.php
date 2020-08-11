eb133d97dc1d1287f738333883cf6b190b5dd403
muhammadrillopandhudhiluhur <pandhumuhammad@gmail.com>
2020-08-11 17:16:28 +0700

    UAS Fitur Dosen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script scr="{{asset('js/app.js')}}"></script>\
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-    
-</body>
-</html>
+
 <html lang="en">
 
 <head>
